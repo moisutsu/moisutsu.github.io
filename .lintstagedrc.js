9b27d00228d91962f31ad475116fc7f9d966406c
moisutsu <moisutsu@gmail.com>
2023-06-10 17:58:40 +0900

    Update eslint and prettier settings/Format all files

---
+++ .lintstagedrc.js
@@ -0,0 +1,11 @@
+// refer: https://zenn.dev/rabbit/scraps/6037df5f89935e
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+}
